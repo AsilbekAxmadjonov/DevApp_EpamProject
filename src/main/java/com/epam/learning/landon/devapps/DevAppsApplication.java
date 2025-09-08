0b60f52126b9219f180fda1482a81c250cb52e4d
Asilbek Axmadjonov <a.axmadjonov@newuu.uz>
2025-09-08 22:41:56 +0500

    first commit

---
+++ src/main/java/com/epam/learning/landon/devapps/DevAppsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.learning.landon.devapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/learning/landon/devapps/DevAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.learning.landon.devapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
